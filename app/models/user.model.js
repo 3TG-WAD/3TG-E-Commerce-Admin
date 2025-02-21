75dea36a1209b92d85b007c17c67ad802c149973
Trương Tâm Thành <86182290+truongtamthanh2004@users.noreply.github.com>
2025-02-21 18:31:33 +0700

    Update user.model.js

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -2,72 +2,6 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const mongoosePaginate = require("mongoose-paginate-v2");
 
-// const UserSchema = new mongoose.Schema(
-//   {
-//     user_id: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//     },
-//     username: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       trim: true,
-//     },
-//     email: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       trim: true,
-//       lowercase: true,
-//     },
-//     password: {
-//       type: String,
-//       required: true,
-//     },
-//     address: {
-//       street: String,
-//       ward: String,
-//       district: String,
-//       city: String,
-//       country: String,
-//       postal_code: String,
-//     },
-//     role: {
-//       type: String,
-//       enum: ["Customer", "Admin"],
-//       default: "Customer",
-//     },
-//     avatar: String,
-//     current_orders: [
-//       {
-//         type: String,
-//       },
-//     ],
-//     is_active: { type: Boolean, default: true },
-//   },
-//   {
-//     timestamps: true, // Tự động thêm created_at và updated_at
-//   }
-// );
-
-// // Middleware để hash mật khẩu trước khi lưu
-// UserSchema.pre("save", async function (next) {
-//   if (this.isModified("password")) {
-//     // Đổi thành snake_case
-//     this.password = await bcrypt.hash(this.password, 10); // Đổi thành snake_case
-//   }
-//   next();
-// });
-
-// // Phương thức kiểm tra mật khẩu
-// UserSchema.methods.comparePassword = async function (candidatePassword) {
-//   return bcrypt.compare(candidatePassword, this.password); // Đổi thành snake_case
-// };
-
-// UserSchema.plugin(mongoosePaginate);
-
 const UserSchema = new mongoose.Schema(
   {
     username: {
@@ -119,10 +53,10 @@ const UserSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    activationToken: String, // Token for account activation
-    activationTokenExpires: Date, // Expiration date for activation token
-    resetPasswordToken: String, // Token for password reset
-    resetPasswordExpires: Date, // Expiration date for reset password token
+    activationToken: String,
+    activationTokenExpires: Date,
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
     authProvider: {
       type: String,
       enum: ["local", "google"],
